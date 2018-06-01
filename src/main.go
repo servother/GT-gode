fc71c44a861fc34bb43f2d0614331dd35823c952
grovetind <36033616+servother@users.noreply.github.com>
2018-06-01 14:49:00 +0800

    main.go

---
+++ src/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"github.com/lxn/walk"
+	. "github.com/lxn/walk/declarative"
+	"strings"
+	"os"
+	"fmt"
+	"io"
+	"gindemo/utils"
+	"os/exec"
+)
+
+//
+func ShutDownEXE() {
+	fmt.Println("关闭主机")
+	arg := []string{"-s", "-t", "20"}
+	cmd := exec.Command("shutdown", arg...)
+	d, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Println(string(d))
+	return
+}
+
+func main() {
+	var inTE, outTE *walk.TextEdit
+
+	MainWindow{
+		Title:   "字母大写转换",
+		MinSize: Size{600, 400},
+		Layout:  VBox{},
+		Children: []Widget{
+			HSplitter{
+				Children: []Widget{
+					TextEdit{AssignTo: &inTE},
+					TextEdit{AssignTo: &outTE, ReadOnly: true},
+				},
+			},
+			PushButton{
+				Text: "转换",
+				OnClicked: func() {
+					outTE.SetText(strings.ToUpper(inTE.Text()))
+					ShutDownEXE()
+				},
+			},
+		},
+	}.Run()
+}
